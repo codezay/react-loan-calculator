9b15ce24d7f1f481b5a45592e356e7af8232329c
Hejo <59996938+DevHejo@users.noreply.github.com>
2022-12-06 12:09:39 +0530

    calculated the data and displayed them

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -7,13 +7,43 @@ const Calculator = () => {
     years: "",
   });
 
+  // function to set the values of the  above state
   const handleInputChange = (event) => {
     setUserValues({ ...userValues, [event.target.name]: event.target.value });
   };
 
+  // function to handle submition
   const handleSubmitValues = (e) => {
     e.preventDefault();
     console.log(userValues);
+    calculateResults(userValues);
+  };
+
+  // function to calculate the data
+  const calculateResults = ({ amount, interest, years }) => {
+    const userAmount = Number(amount);
+    const calculatedInterest = Number(interest) / 100 / 12;
+    const calculatedPayments = Number(years) * 12;
+    const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+    const monthly = (userAmount * x * calculatedInterest) / (x - 1);
+
+    if (isFinite(monthly)) {
+      const monthlyPaymentCalculated = monthly.toFixed(2);
+      const totalPaymentCalculated = (monthly * calculatedPayments).toFixed(2);
+      const totalInterestCalculated = (
+        monthly * calculatedPayments -
+        userAmount
+      ).toFixed(2);
+
+      setResults({
+        monthlyPayment: monthlyPaymentCalculated,
+        totalPayment: totalPaymentCalculated,
+        totalInterest: totalInterestCalculated,
+        isResult: true,
+      });
+    }
+
+    return;
   };
 
   const [results, setResults] = useState({
@@ -26,39 +56,81 @@ const Calculator = () => {
   return (
     <div>
       <form onSubmit={handleSubmitValues}>
-        <div>
+        {!results.isResult ? (
+          // Form to submit data
           <div>
-            <label>Amount:</label>
-            <input
-              type="text"
-              name="amount"
-              placeholder="Loan amount"
-              value={userValues.amount}
-              onChange={handleInputChange}
-            />
+            <div>
+              <label>Amount:</label>
+              <input
+                type="text"
+                name="amount"
+                placeholder="Loan amount"
+                value={userValues.amount}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="">Interest</label>
+              <input
+                type="text"
+                name="interest"
+                placeholder="Interest"
+                value={userValues.interest}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="">Years</label>
+              <input
+                type="text"
+                name="years"
+                placeholder="Years"
+                value={userValues.years}
+                onChange={handleInputChange}
+              />
+            </div>
+            <input type="submit" />
           </div>
+        ) : (
+          // Form to display data
           <div>
-            <label htmlFor="">Interest</label>
-            <input
-              type="text"
-              name="interest"
-              placeholder="Interest"
-              value={userValues.interest}
-              onChange={handleInputChange}
-            />
+            <h4>
+              Loan amount: Rs {userValues.amount} <br />
+              Interest: {userValues.interest} <br />
+              Years to repay: {userValues.years}
+            </h4>
+            <div>
+              <label htmlFor="">Monthly Payment:</label>
+              <input
+                type="text"
+                name=""
+                id=""
+                value={results.monthlyPayment}
+                disabled
+              />
+            </div>
+            <div>
+              <label htmlFor="">Total Payment:</label>
+              <input
+                type="text"
+                name=""
+                id=""
+                value={results.totalPayment}
+                disabled
+              />
+            </div>
+            <div>
+              <label htmlFor="">Total Interest:</label>
+              <input
+                type="text"
+                name=""
+                id=""
+                value={results.totalInterest}
+                disabled
+              />
+            </div>
           </div>
-          <div>
-            <label htmlFor="">Years</label>
-            <input
-              type="text"
-              name="years"
-              placeholder="Years"
-              value={userValues.years}
-              onChange={handleInputChange}
-            />
-          </div>
-          <input type="submit" />
-        </div>
+        )}
       </form>
     </div>
   );
