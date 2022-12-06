db558a0a5ec7cc6f144eec3618cc5d9f6a7b67d3
Hejo <59996938+DevHejo@users.noreply.github.com>
2022-12-06 11:33:03 +0530

    fetching data from form

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -8,21 +8,56 @@ const Calculator = () => {
   });
 
   const handleInputChange = (event) => {
-    // 
-  }
+    setUserValues({ ...userValues, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmitValues = (e) => {
+    e.preventDefault();
+    console.log(userValues);
+  };
+
+  const [results, setResults] = useState({
+    monthlyPayment: "",
+    totalPayment: "",
+    totalInterest: "",
+    isResult: false,
+  });
 
   return (
     <div>
-      <form action="">
+      <form onSubmit={handleSubmitValues}>
         <div>
           <div>
             <label>Amount:</label>
-            <input type="text" name="amount" placeholder="Loan amount" value={userValues.amount} onChange={handleInputChange}/>
+            <input
+              type="text"
+              name="amount"
+              placeholder="Loan amount"
+              value={userValues.amount}
+              onChange={handleInputChange}
+            />
           </div>
           <div>
             <label htmlFor="">Interest</label>
-            <input type="text" name="interest" placeholder="Interest" value={userValues.interest} onChange={handleInputChange}/>
+            <input
+              type="text"
+              name="interest"
+              placeholder="Interest"
+              value={userValues.interest}
+              onChange={handleInputChange}
+            />
           </div>
+          <div>
+            <label htmlFor="">Years</label>
+            <input
+              type="text"
+              name="years"
+              placeholder="Years"
+              value={userValues.years}
+              onChange={handleInputChange}
+            />
+          </div>
+          <input type="submit" />
         </div>
       </form>
     </div>
