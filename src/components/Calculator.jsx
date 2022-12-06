6468dd15b20e2e8a9f27fbfbc800548b5ad3b9c2
Hejo <59996938+DevHejo@users.noreply.github.com>
2022-12-06 13:09:42 +0530

    added a way to clear the data and start from begining

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -16,7 +16,10 @@ const Calculator = () => {
   const handleSubmitValues = (e) => {
     e.preventDefault();
     console.log(userValues);
-    calculateResults(userValues);
+    if (isvalid()) {
+      setError("");
+      calculateResults(userValues);
+    }
   };
 
   // function to calculate the data
@@ -53,85 +56,136 @@ const Calculator = () => {
     isResult: false,
   });
 
+  const [error, setError] = useState("");
+
+  const isvalid = () => {
+    const { amount, interest, years } = userValues;
+    let actualError = "";
+
+    if (!amount || !interest || !years) {
+      actualError = "All the Fields are required";
+    }
+
+    if (isNaN(amount) || isNaN(interest) || isNaN(years)) {
+      actualError = "All Values must be a number";
+    }
+
+    if (Number(amount) <= 0 || Number(interest) <= 0 || Number(years) <= 0) {
+      actualError = "All numbers must be positive and greater than zero";
+    }
+
+    if (actualError) {
+      setError(actualError);
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  const clearFields = () => {
+    setUserValues({
+      amount: "",
+      interest: "",
+      years: "",
+    });
+
+    setResults({
+      monthlyPayment: "",
+      totalPayment: "",
+      totalInterest: "",
+      isResult: false,
+    });
+  };
+
   return (
-    <div>
-      <form onSubmit={handleSubmitValues}>
-        {!results.isResult ? (
-          // Form to submit data
-          <div>
-            <div>
-              <label>Amount:</label>
+    <div className="calculator">
+      <div className="form">
+        <h1 className="test">Loan Calculator</h1>
+        <p className="error">{error}</p>
+        <form onSubmit={handleSubmitValues}>
+          {!results.isResult ? (
+            // Form to submit data
+            <div className="form-items">
+              <div>
+                <label id="label">Amount:</label>
+                <input
+                  type="text"
+                  name="amount"
+                  placeholder="Loan amount"
+                  value={userValues.amount}
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div>
+                <label id="label">Interest</label>
+                <input
+                  type="text"
+                  name="interest"
+                  placeholder="Interest"
+                  value={userValues.interest}
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div>
+                <label id="label">Years</label>
+                <input
+                  type="text"
+                  name="years"
+                  placeholder="Years"
+                  value={userValues.years}
+                  onChange={handleInputChange}
+                />
+              </div>
+              <input type="submit" />
+            </div>
+          ) : (
+            // Form to display data
+            <div className="form-items">
+              <h4>
+                Loan amount: Rs {userValues.amount} <br />
+                Interest: {userValues.interest} <br />
+                Years to repay: {userValues.years}
+              </h4>
+              <div>
+                <label id="label">Monthly Payment:</label>
+                <input
+                  type="text"
+                  name=""
+                  id=""
+                  value={results.monthlyPayment}
+                  disabled
+                />
+              </div>
+              <div>
+                <label id="label">Total Payment:</label>
+                <input
+                  type="text"
+                  name=""
+                  id=""
+                  value={results.totalPayment}
+                  disabled
+                />
+              </div>
+              <div>
+                <label id="label">Total Interest:</label>
+                <input
+                  type="text"
+                  name=""
+                  id=""
+                  value={results.totalInterest}
+                  disabled
+                />
+              </div>
               <input
-                type="text"
-                name="amount"
-                placeholder="Loan amount"
-                value={userValues.amount}
-                onChange={handleInputChange}
+                type="button"
+                value="Clear"
+                className="button"
+                onClick={clearFields}
               />
             </div>
-            <div>
-              <label htmlFor="">Interest</label>
-              <input
-                type="text"
-                name="interest"
-                placeholder="Interest"
-                value={userValues.interest}
-                onChange={handleInputChange}
-              />
-            </div>
-            <div>
-              <label htmlFor="">Years</label>
-              <input
-                type="text"
-                name="years"
-                placeholder="Years"
-                value={userValues.years}
-                onChange={handleInputChange}
-              />
-            </div>
-            <input type="submit" />
-          </div>
-        ) : (
-          // Form to display data
-          <div>
-            <h4>
-              Loan amount: Rs {userValues.amount} <br />
-              Interest: {userValues.interest} <br />
-              Years to repay: {userValues.years}
-            </h4>
-            <div>
-              <label htmlFor="">Monthly Payment:</label>
-              <input
-                type="text"
-                name=""
-                id=""
-                value={results.monthlyPayment}
-                disabled
-              />
-            </div>
-            <div>
-              <label htmlFor="">Total Payment:</label>
-              <input
-                type="text"
-                name=""
-                id=""
-                value={results.totalPayment}
-                disabled
-              />
-            </div>
-            <div>
-              <label htmlFor="">Total Interest:</label>
-              <input
-                type="text"
-                name=""
-                id=""
-                value={results.totalInterest}
-                disabled
-              />
-            </div>
-          </div>
-        )}
-      </form>
+          )}
+        </form>
+      </div>
     </div>
   );
 };
